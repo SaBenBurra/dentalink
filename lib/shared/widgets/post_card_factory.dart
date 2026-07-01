710d3108b688adc9b052febbfebe6d4d23d4fdb4
sabenburra <gezerbugra@gmail.com>
2026-07-01 21:05:04 +0300

    post_card_factory updated

--- lib/shared/widgets/post_card_factory.dart
+++ lib/shared/widgets/post_card_factory.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:dentlink/data/models/post_model.dart'; // kendi yoluna göre düzelt
+import 'package:dentlink/data/models/post_model.dart';
 import 'package:dentlink/data/models/enums.dart';
 import 'package:dentlink/shared/widgets/case_card.dart';
 import 'package:dentlink/shared/widgets/question_card.dart';
 
-/// Feed ve Profil'in ortak kullandığı tek "PostType -> kart" dönüşüm noktası.
 class PostCardFactory {
   const PostCardFactory._();
 
+  // Sadece build işlemlerinden sorumlu. Router ve Provider bağımsız. // <--
   static Widget build(
     PostModel post, {
     required VoidCallback onLikeToggle,
     required VoidCallback onBookmarkToggle,
+    VoidCallback? onCommentTap,
+    VoidCallback? onTap,
   }) {
     return switch (post.type) {
       PostType.casePost => CaseCard(
         post: post,
         onLikeToggle: onLikeToggle,
         onBookmarkToggle: onBookmarkToggle,
+        onCommentTap: onCommentTap, // <-- Parametreden gelen değer
+        onTap: onTap, // <-- Parametreden gelen değer
       ),
       PostType.question => QuestionCard(
         post: post,
         onLikeToggle: onLikeToggle,
         onBookmarkToggle: onBookmarkToggle,
+        onCommentTap: onCommentTap, // <-- Parametreden gelen değer
+        onTap: onTap,
       ),
     };
   }
